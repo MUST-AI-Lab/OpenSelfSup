42ebf5cab9281bb88469e53b1f1369639152325c
ETBox <13318972468@126.com>
2020-09-09 20:42:31 +0800

    feat: reset moco config

--- configs/selfsup/moco/r50_v1_cifar.py
+++ configs/selfsup/moco/r50_v1_cifar.py
@@ -26,68 +26,36 @@ data_source_cfg = dict(type='Cifar10', root='data')
 #     mclient_path='/mnt/lustre/share/memcached_client')
 # data_train_list = 'data/imagenet/meta/train.txt'
 # data_train_root = 'data/imagenet/train'
-dataset_type = 'ClassificationDataset'
-# dataset_type = 'ContrastiveDataset'
-img_norm_cfg = dict(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.201])
-# img_norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+dataset_type = 'ContrastiveDataset'
+img_norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 train_pipeline = [
-    dict(type='RandomCrop', size=32, padding=4),
+    dict(type='RandomResizedCrop', size=224, scale=(0.2, 1.)),
+    dict(type='RandomGrayscale', p=0.2),
+    dict(
+        type='ColorJitter',
+        brightness=0.4,
+        contrast=0.4,
+        saturation=0.4,
+        hue=0.4),
     dict(type='RandomHorizontalFlip'),
     dict(type='ToTensor'),
     dict(type='Normalize', **img_norm_cfg),
 ]
-# train_pipeline = [
-#     dict(type='RandomResizedCrop', size=224, scale=(0.2, 1.)),
-#     dict(type='RandomGrayscale', p=0.2),
-#     dict(
-#         type='ColorJitter',
-#         brightness=0.4,
-#         contrast=0.4,
-#         saturation=0.4,
-#         hue=0.4),
-#     dict(type='RandomHorizontalFlip'),
-#     dict(type='ToTensor'),
-#     dict(type='Normalize', **img_norm_cfg),
-# ]
-
-test_pipeline = [
-    dict(type='ToTensor'),
-    dict(type='Normalize', **img_norm_cfg),
-]
-
 data = dict(
-    imgs_per_gpu=128,
-    workers_per_gpu=2,
+    imgs_per_gpu=32,  # total 32*8=256
+    workers_per_gpu=4,
+    drop_last=True,
     train=dict(
         type=dataset_type,
-        data_source=dict(split='train', **data_source_cfg),
-        pipeline=train_pipeline),
-    val=dict(
-        type=dataset_type,
-        data_source=dict(split='test', **data_source_cfg),
-        pipeline=test_pipeline),
-    test=dict(
-        type=dataset_type,
-        data_source=dict(split='test', **data_source_cfg),
-        pipeline=test_pipeline))
-# data = dict(
-#     imgs_per_gpu=32,  # total 32*8=256
-#     workers_per_gpu=4,
-#     drop_last=True,
-#     train=dict(
-#         type=dataset_type,
-#         data_source=dict(
-#             list_file=data_train_list, root=data_train_root,
-#             **data_source_cfg),
-#         pipeline=train_pipeline))
+        data_source=dict(
+            # list_file=data_train_list, root=data_train_root,
+            split='train',
+            **data_source_cfg),
+        pipeline=train_pipeline))
 # Optimizer
-optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0005)
-# optimizer = dict(type='SGD', lr=0.03, weight_decay=0.0001, momentum=0.9)
+optimizer = dict(type='SGD', lr=0.03, weight_decay=0.0001, momentum=0.9)
 # Learning policy
-lr_config = dict(policy='step', step=[150, 250])
-# lr_config = dict(policy='step', step=[120, 160])
-checkpoint_config = dict(interval=50)
-# checkpoint_config = dict(interval=10)
+lr_config = dict(policy='step', step=[120, 160])
+checkpoint_config = dict(interval=10)
 # Runtime settings
-total_epochs = 350
-# total_epochs = 200
+total_epochs = 200
--- openselfsup/version.py
+++ openselfsup/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Sun Aug 30 11:45:34 2020
+# TIME: Mon Sep  7 16:34:03 2020
 
-__version__ = '0.2.0+dbfc6b1'
+__version__ = '0.2.0+71a54eb'
 short_version = '0.2.0'
