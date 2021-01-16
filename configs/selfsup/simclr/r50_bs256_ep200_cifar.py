a11463ce4d629c0773a2fe4d233c11cb5e1427b0
ETBox <13318972468@126.com>
2021-01-16 11:05:57 +0800

    refactor: mostly use SimCLR but fail in dataloader

--- configs/selfsup/simclr/r50_bs256_ep200_cifar.py
+++ configs/selfsup/simclr/r50_bs256_ep200_cifar.py
@@ -1,43 +1,71 @@
 _base_ = '../../base.py'
 # model settings
-model = dict(
-    type='SimCLR', ## use SimCLR model
+model = dict( ## totally use SimCLR model
+    type='SimCLR',
     pretrained=None,
     backbone=dict(
         type='ResNet',
         depth=50,
-        out_indices=[4],  # 4: stage-4
-        norm_cfg=dict(type='BN')),
-    neck=dict( ## SimCLR requires neck
+        in_channels=3,
+        out_indices=[4],  # 0: conv-1, x: stage-x
+        norm_cfg=dict(type='SyncBN')),
+    neck=dict(
         type='NonLinearNeckSimCLR', # SimCLR non-linear neck
         in_channels=2048,
         hid_channels=2048,
         out_channels=128,
         num_layers=2,
         with_avg_pool=True),
-    head=dict(
-        type='ClsHead', with_avg_pool=True, in_channels=2048, num_classes=10))
+    head=dict(type='ContrastiveHead', temperature=0.1))
 # dataset settings
-data_source_cfg = dict(type='Cifar10', root='data')
-dataset_type = 'ClassificationDataset'
-img_norm_cfg = dict(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.201])
-train_pipeline = [
-    dict(type='RandomCrop', size=32, padding=4),
+data_source_cfg = dict(type='Cifar10', root='data') ## classification data_source_cfg
+dataset_type = 'ContrastiveDataset' ## SimCLR dataset_type
+img_norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) ## SimCLR img_norm_cfg
+train_pipeline = [ ## SimCLR train_pipeline
+    dict(type='RandomResizedCrop', size=224),
     dict(type='RandomHorizontalFlip'),
+    dict(
+        type='RandomAppliedTrans',
+        transforms=[
+            dict(
+                type='ColorJitter',
+                brightness=0.8,
+                contrast=0.8,
+                saturation=0.8,
+                hue=0.2)
+        ],
+        p=0.8),
+    dict(type='RandomGrayscale', p=0.2),
+    dict(
+        type='RandomAppliedTrans',
+        transforms=[
+            dict(
+                type='GaussianBlur',
+                sigma_min=0.1,
+                sigma_max=2.0)
+        ],
+        p=0.5),
+]
+test_pipeline = [ ## classification test_pipeline
     dict(type='ToTensor'),
     dict(type='Normalize', **img_norm_cfg),
 ]
-test_pipeline = [
-    dict(type='ToTensor'),
-    dict(type='Normalize', **img_norm_cfg),
-]
-data = dict(
+
+# prefetch
+prefetch = False
+if not prefetch:
+    train_pipeline.extend([dict(type='ToTensor'), dict(type='Normalize', **img_norm_cfg)])
+
+data = dict( ## classification data with val & test
     imgs_per_gpu=128,
     workers_per_gpu=2,
-    train=dict(
+    train=dict( ## SimCLR style training with prefetch
         type=dataset_type,
-        data_source=dict(split='train', **data_source_cfg),
-        pipeline=train_pipeline),
+        data_source=dict(
+            split='train', **data_source_cfg),
+        pipeline=train_pipeline,
+        prefetch=prefetch,
+    ),
     val=dict(
         type=dataset_type,
         data_source=dict(split='test', **data_source_cfg),
@@ -47,7 +75,7 @@ data = dict(
         data_source=dict(split='test', **data_source_cfg),
         pipeline=test_pipeline))
 # additional hooks
-custom_hooks = [
+custom_hooks = [ ## classification custom_hooks
     dict(
         type='ValidateHook',
         dataset=data['val'],
@@ -58,9 +86,22 @@ custom_hooks = [
         eval_param=dict(topk=(1, 5)))
 ]
 # optimizer
-optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0005)
+optimizer = dict( ## SimCLR optimizer
+    type='LARS',
+    lr=0.3,
+    weight_decay=0.000001,
+    momentum=0.9,
+    paramwise_options={
+        r'(bn|gn)(\d+)?.(weight|bias)': dict(weight_decay=0., lars_exclude=True),
+        'bias': dict(weight_decay=0., lars_exclude=True)})
 # learning policy
-lr_config = dict(policy='step', step=[150, 250])
-checkpoint_config = dict(interval=50)
+lr_config = dict( ## SimCLR lr_config
+    policy='CosineAnnealing',
+    min_lr=0.,
+    warmup='linear',
+    warmup_iters=10,
+    warmup_ratio=0.0001,
+    warmup_by_epoch=True)
+checkpoint_config = dict(interval=10) ## SimCLR checkpoint_config
 # runtime settings
-total_epochs = 350
+total_epochs = 200 ## SimCLR total_epochs
