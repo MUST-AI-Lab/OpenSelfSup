84a74f93ef6487bc76e2642c25a9939d54591a4e
ETBox <13318972468@126.com>
2021-01-21 00:23:31 +0800

    feat: train rotation_pred with cifar10

--- configs/selfsup/rotation_pred/r50_cifar.py
+++ configs/selfsup/rotation_pred/r50_cifar.py
@@ -1,5 +1,5 @@
 _base_ = '../../base.py'
-# Model settings
+# model settings
 model = dict(
     type='RotationPred',
     pretrained=None,
@@ -11,16 +11,8 @@ model = dict(
         norm_cfg=dict(type='SyncBN')),
     head=dict(
         type='ClsHead', with_avg_pool=True, in_channels=2048, num_classes=4))
-# Dataset settings
-data_source_cfg = dict(type='Cifar10', root='data')
-# data_source_cfg = dict(
-#     type='ImageNet',
-#     memcached=True,
-#     mclient_path='/mnt/lustre/share/memcached_client')
-# data_train_list = 'data/imagenet/meta/train.txt'
-# data_train_root = 'data/imagenet/train'
-# data_test_list = 'data/imagenet/meta/val.txt'
-# data_test_root = 'data/imagenet/val'
+# dataset settings
+data_source_cfg = dict(type='Cifar10', root='data') ## classification data_source_cfg
 dataset_type = 'RotationPredDataset'
 img_norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 train_pipeline = [
@@ -41,21 +33,19 @@ data = dict(
     train=dict(
         type=dataset_type,
         data_source=dict(
-            # list_file=data_train_list, root=data_train_root,
             split='train',
             **data_source_cfg),
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
         data_source=dict(
-            # list_file=data_test_list, root=data_test_root,
             split='test',
             **data_source_cfg),
         pipeline=test_pipeline))
-# Optimizer
+# optimizer
 optimizer = dict(
     type='SGD', lr=0.2, momentum=0.9, weight_decay=0.0001, nesterov=False)
-# Learning policy
+# learning policy
 lr_config = dict(
     policy='step',
     step=[30, 50],
@@ -64,5 +54,5 @@ lr_config = dict(
     warmup_ratio=0.1,
     warmup_by_epoch=True)
 checkpoint_config = dict(interval=10)
-# Runtime settings
+# runtime settings
 total_epochs = 70
