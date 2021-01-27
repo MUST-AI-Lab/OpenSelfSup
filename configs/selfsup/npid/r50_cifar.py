451d2e0ab871f0a867ac6b1f12b5afaf29ffa0ba
ETBox <13318972468@126.com>
2021-01-27 20:13:49 +0800

    feat: train npid with cifar10

--- configs/selfsup/npid/r50_cifar.py
+++ configs/selfsup/npid/r50_cifar.py
@@ -1,5 +1,5 @@
 _base_ = '../../base.py'
-# Model settings
+# model settings
 model = dict(
     type='NPID',
     neg_num=65536,
@@ -18,14 +18,8 @@ model = dict(
     head=dict(type='ContrastiveHead', temperature=0.07),
     memory_bank=dict(
         type='SimpleMemory', length=1281167, feat_dim=128, momentum=0.5))
-# Dataset settings
-data_source_cfg = dict(type='Cifar10', root='data')
-# data_source_cfg = dict(
-#     type='ImageNet',
-#     memcached=True,
-#     mclient_path='/mnt/lustre/share/memcached_client')
-# data_train_list = 'data/imagenet/meta/train.txt'
-# data_train_root = 'data/imagenet/train'
+# dataset settings
+data_source_cfg = dict(type='Cifar10', root='data') ## classification data_source_cfg
 dataset_type = 'NPIDDataset'
 img_norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 train_pipeline = [
@@ -53,15 +47,14 @@ data = dict(
     train=dict(
         type=dataset_type,
         data_source=dict(
-            # list_file=data_train_list, root=data_train_root,
             split='train',
             **data_source_cfg),
-        pipeline=train_pipeline))
-# Optimizer
+        pipeline=train_pipeline)) ## need extra benchmark
+# optimizer
 optimizer = dict(
     type='SGD', lr=0.03, weight_decay=0.0001, momentum=0.9, nesterov=False)
-# Learning policy
+# learning policy
 lr_config = dict(policy='step', step=[120, 160])
 checkpoint_config = dict(interval=10)
-# Runtime settings
+# runtime settings
 total_epochs = 200
