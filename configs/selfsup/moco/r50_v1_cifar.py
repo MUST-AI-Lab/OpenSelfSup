1ee269cdaea0725968310b2d9633539c73bc2ef8
ETBox <13318972468@126.com>
2021-02-10 11:45:26 +0800

    refactor: update 2 moco with cifar10

--- configs/selfsup/moco/r50_v1_cifar.py
+++ configs/selfsup/moco/r50_v1_cifar.py
@@ -1,5 +1,5 @@
 _base_ = '../../base.py'
-# Model settings
+# model settings
 model = dict(
     type='MOCO',
     pretrained=None,
@@ -18,14 +18,8 @@ model = dict(
         out_channels=128,
         with_avg_pool=True),
     head=dict(type='ContrastiveHead', temperature=0.07))
-# Dataset settings
-data_source_cfg = dict(type='Cifar10', root='data')
-# data_source_cfg = dict(
-#     type='ImageNet',
-#     memcached=True,
-#     mclient_path='/mnt/lustre/share/memcached_client')
-# data_train_list = 'data/imagenet/meta/train.txt'
-# data_train_root = 'data/imagenet/train'
+# dataset settings
+data_source_cfg = dict(type='Cifar10', root='data') ## classification data_source_cfg
 dataset_type = 'ContrastiveDataset'
 img_norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 train_pipeline = [
@@ -38,9 +32,12 @@ train_pipeline = [
         saturation=0.4,
         hue=0.4),
     dict(type='RandomHorizontalFlip'),
-    dict(type='ToTensor'),
-    dict(type='Normalize', **img_norm_cfg),
 ]
+# prefetch
+prefetch = False
+if not prefetch:
+    train_pipeline.extend([dict(type='ToTensor'), dict(type='Normalize', **img_norm_cfg)])
+
 data = dict(
     imgs_per_gpu=32,  # total 32*8=256
     workers_per_gpu=4,
@@ -48,14 +45,15 @@ data = dict(
     train=dict(
         type=dataset_type,
         data_source=dict(
-            # list_file=data_train_list, root=data_train_root,
             split='train',
             **data_source_cfg),
-        pipeline=train_pipeline))
-# Optimizer
+        pipeline=train_pipeline,
+        prefetch=prefetch,
+    ))
+# optimizer
 optimizer = dict(type='SGD', lr=0.03, weight_decay=0.0001, momentum=0.9)
-# Learning policy
+# learning policy
 lr_config = dict(policy='step', step=[120, 160])
 checkpoint_config = dict(interval=10)
-# Runtime settings
+# runtime settings
 total_epochs = 200
